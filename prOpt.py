b64c8d78f28382485c4ccecaba14efc1696a68c8
sberton2 <stefano.bertone@nasa.gov>
2020-03-06 12:25:42 -0500

    04/03/2020 SB: modify

--- prOpt.py
+++ prOpt.py
@@ -5,7 +5,7 @@ from util import deg2as
 ##############################################
 # @profile
 # local or PGDA
-local = 0
+local = 1
 # debug mode
 debug = 0
 # parallel processing?
@@ -14,7 +14,7 @@ parallel = 1
 partials = 1
 
 # std perturbations for finite differences (dRl, dPt, dRA, dDEC, dL in arcsec; dPM in arcsec/Julian year)
-parOrb = {'dA': 20.,'dC': 20.,'dR': 5.} #,'dRl':0.4, 'dPt':0.4} #
+parOrb = {} #'dA': 20.,'dC': 20.,'dR': 5.} #,'dRl':0.4, 'dPt':0.4} #
 parGlo = {'dRA':[0.2, 0.000, 0.000], 'dDEC':[0.36, 0.000, 0.000],'dPM':[0, 0.013, 0.000],'dL':1.e-3*deg2as(1.)*np.linalg.norm([0.00993822,-0.00104581,-0.00010280,-0.00002364,-0.00000532]), 'dh2': 0.1}
 
 # parameter constraints for solution
@@ -31,16 +31,16 @@ pert_tracks = [] #'1107021838','1210192326','1403281002','1503191143'] #
 
 # select subset of parameters to solve for
 sol4_orb = [] #'1107021838','1210192326','1403281002','1503191143']  #
-sol4_orbpar = ['dA','dC','dR'] #,'dA1','dC1','dR1','dA2','dC2','dR2']  #] #
+sol4_orbpar = [None] #'dA','dC','dR'] #,'dRl','dPt'] #,'dA1','dC1','dR1','dA2','dC2','dR2']  #] #
 sol4_glo = ['dR/dRA', 'dR/dDEC', 'dR/dPM','dR/dL','dR/dh2'] #,  None]
 
 # orbital representation
 OrbRep = 'cnt' # 'lin' # 'quad' #
-# interpolation/spice direct call (0:no, 1:yes, use, 2: yes, create)
+# interpolation/spice direct call (0: use spice, 1: yes, use interpolation, 2: yes, create interpolation)
 SpInterp = 1
-# create new gtrack (0:no, 1:yes, check if present, 2: yes, create)
-new_gtrack = 2
-# create new xov (0:no, 1:yes, check if present, 2: yes, create)
+# create new gtrack (0:no, 1:yes, if not already present, 2: yes, create and replace)
+new_gtrack = 1
+# create new xov (0:no, 1:yes, if not already present, 2: yes, create and replace)
 new_xov = 2
 
 # Other options
