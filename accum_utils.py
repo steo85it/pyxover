29.05.2020 SB: corrected variance to std, ext_iter > 0 for converged so h2

accum_utils.py
@@ -397,7 +397,7 @@ def analyze_sol(xovi_amat,xov,mode='full'):
                    np.reshape(xovi_amat.sol[0], (-1, 1)),
                    np.reshape(xovi_amat.sol[-1], (-1, 1))
                    ))
-    sol_dict = {'sol': dict(zip(_[:,0],_[:,1].astype(float))), 'std': dict(zip(_[:,0],_[:,2].astype(float))) }
+    sol_dict = {'sol': dict(zip(_[:,0],_[:,1].astype(float))), 'std': dict( zip( _[:,0],np.sqrt(_[:,2].astype(float))) ) }
 
     if debug:
         print("sol_dict_analyze_sol")