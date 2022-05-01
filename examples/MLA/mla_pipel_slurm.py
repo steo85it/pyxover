e4d79617d5e95df76447dfe1c0c542e7fa20cdb7
Stefano Bertone <stefano.bertone@nasa.gov>
2022-05-01 17:22:29 -0400

    slurm pipeline

--- examples/MLA/mla_pipel_slurm.py
+++ examples/MLA/mla_pipel_slurm.py
@@ -14,25 +14,25 @@ from pyxover import PyXover
 
 # PyTest requires parallel = False
 
-conf = XovOpt()
+options = XovOpt()
 # update paths and check options
-conf.set("basedir", 'data/')
-conf.set("instrument", 'MLA')
-conf.set("local", False)
-conf.set("parallel", False)
-conf.set("expopt", 'AA2')
+options.set("basedir", 'data/')
+options.set("instrument", 'MLA')
+options.set("local", False)
+options.set("parallel", False)
+options.set("expopt", 'AA2')
 
-conf.set("new_gtrack", 1)
-vecopts = conf.get('vecopts')
+options.set("new_gtrack", 1)
+vecopts = options.get('vecopts')
 vecopts['SCFRAME'] = 'MSGR_SPACECRAFT'
-conf.set('vecopts', vecopts)
-conf.set("SpInterp", 0)
+options.set('vecopts', vecopts)
+options.set("SpInterp", 0)
 
-conf.check_consistency()
+options.check_consistency()
 AccOpt.check_consistency()
 
 # os.chdir("MLA/")
-opt_dict = conf.to_dict()
+opt_dict = options.to_dict()
 
 iternum = 0
 pygeoloc_in = [[f'{yy:02d}{mm:02d}', f'SIM_{yy:02d}/KX5/0res_1amp/', f'sim/{XovOpt.get("expopt")}_{iternum}/0res_1amp/gtrack_{yy:02d}', 'MLASCIRDR', iternum, opt_dict] for mm in range(13)[1:] for yy in [8,11,12,13,14,15]]
@@ -49,15 +49,15 @@ executor.update_parameters( #slurm_account='j1010',
                            slurm_mem='8G',
                            slurm_array_parallelism=50)
 
-if True:
+if False:
     #job = executor.submit(PyGeoloc.main, pygeoloc_in[0]) # single job
     jobs = executor.map_array(PyGeoloc.main, pygeoloc_in)
     for job in jobs:
         print(job.result())
 
 # run PyXover
-if True:
-    conf.set("parallel", True)
+if False:
+    options.set("parallel", True)
     executor.update_parameters(slurm_name="pyxover",
                                slurm_cpus_per_task=10,
                                slurm_mem='90G',
@@ -68,5 +68,15 @@ if True:
     for job in jobs:
         print(job.result())
 
-if False:
-    out = AccumXov.main([[f'sim/{XovOpt.get("expopt")}_{iternum}/0res_1amp/'], 'sim', iternum])
+if True:
+    options.set("parallel", True)
+    options.set("sol4_orb", [])
+    options.set("sol4_orbpar", ['dA','dC','dR'])
+    executor.update_parameters(slurm_name="accumxov",
+                               slurm_cpus_per_task=10,
+                               slurm_mem='90G',
+                               slurm_array_parallelism=2)
+
+    # job = AccumXov.main([[f'sim/{XovOpt.get("expopt")}_{iternum}/0res_1amp/'], 'sim', iternum])
+    job = executor.submit(AccumXov.main, [[f'sim/{XovOpt.get("expopt")}_{iternum}/0res_1amp/'], 'sim', iternum, opt_dict]) # single job
+    print(job.result())
