f46dc9fcf4c3c292c61079038d7fab1939d0c5f2
wdesprats <william.desprats@gmail.com>
2023-02-23 19:02:40 +0100

    Fix for continous ground track simulation

--- src/pyaltsim/prepro.py
+++ src/pyaltsim/prepro.py
@@ -50,14 +50,14 @@ def prepro_BELA_sim(epo_in):
                                XovOpt.get("vecopts")['PLANETNAME'])
 
     scpos = np.array(scpv)[:,:3]
-    range = np.linalg.norm(scpos,axis=1) - XovOpt.get("vecopts")['PLANETRADIUS']
+    range_val = np.linalg.norm(scpos,axis=1) - XovOpt.get("vecopts")['PLANETRADIUS']
 
     scplavec = scpos/np.linalg.norm(scpos,axis=1)[:,None]
-    approx_bounce_point = scplavec*XovOpt.get("vecopts")['PLANETRADIUS'] #range[:,None]
+    approx_bounce_point = scplavec*XovOpt.get("vecopts")['PLANETRADIUS'] #range_val[:,None]
 
     df_ = pd.DataFrame(approx_bounce_point,columns=['x','y','z'])
     df_['epo_tx'] = epo_in
-    df_['rng'] = range
+    df_['rng'] = range_val
 
     approx_bounce_point_sph = astr.cart2sph(approx_bounce_point)
     df_['lat']= np.rad2deg(approx_bounce_point_sph[1]) # pd.DataFrame(approx_bounce_point_sph,columns=['r','lat','lon'])
@@ -76,8 +76,6 @@ def prepro_BELA_sim(epo_in):
 
     # for BELA, new arc at every upwards passage of equator
     df_['diff'] = df_.lat.diff().fillna(0)
-    # print(df_[(df_['diff'] > 0)])
-    # print(df_[(df_['diff'] > 0) & (df_['lat'].round(1) == 0)])
 
     def ranges(nums):
         nums = sorted(set(nums))
@@ -88,9 +86,16 @@ def prepro_BELA_sim(epo_in):
     # set up arc boundaries (and remove consecutive indexes due to approx)
     arcbnd = [df_.index.min()]
     arcbnd.extend(df_[(df_['diff'] > 0) & (df_['lat'].round(0) == 0)].index.values)
-    arcbnd = [x[0] for x in ranges(arcbnd)]
+    ranges_arcbnd = ranges(arcbnd)
+    arcbnd = [ranges_arcbnd[0][0]]
+    # Look for first sign change in ranges_arcbnd
+    for i,j in ranges_arcbnd[1:][:]:
+        for k in range(i+1,j):
+            if df_.loc[i, 'lat']*df_.loc[k, 'lat']<0:
+                arcbnd.append(k)
+                break
+    # arcbnd = [x[0] for x in ranges(arcbnd)]
     arcbnd.extend([df_.index.max() + 1])
-    # print(arcbnd)
 
     df_['orbID'] = 0
     for i, j in zip(arcbnd, arcbnd[1:]):
