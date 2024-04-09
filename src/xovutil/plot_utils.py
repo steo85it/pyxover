985f8cf8c031dd984037183860534eb0fcedf120
wdesprats <william.desprats@gmail.com>
2024-04-09 14:51:14 +0200

    Fixed compatibility issues.

--- src/xovutil/plot_utils.py
+++ src/xovutil/plot_utils.py
@@ -14,35 +14,39 @@ def plt_histo_dR(idx, xov_df, xov_ref='', xlim=None):
     if xlim == None:
         xlim = 50
 
-    plt.figure(figsize=(8,3))
-    plt.xlim(-1.*xlim, xlim)
+    plt.figure(figsize=(8,4))
     # the histogram of the data
-    num_bins = 200 # 'auto'
-    n, bins, patches = plt.hist(xov_df.dR.astype(float), bins=num_bins, density=False, facecolor='blue',
-                                alpha=0.9, range=[-1.*xlim, xlim])
+    num_bins = 500 # 'auto'
+    n, bins, patches = plt.hist(xov_df.dR.astype(float), bins=num_bins, density=False, facecolor='red',
+                                alpha=0.8, range=[-1.*xlim, xlim],label='pre-fit')
     # add a 'best fit' line
     # y = stats.norm.pdf(bins, mean_dR, std_dR)
     # plt.plot(bins, y, 'b--')
-    if isinstance(xov_ref, pd.DataFrame):
-        xov_ref = xov_ref.loc[xov_ref.dR.abs() < 200]
-        n, bins, patches = plt.hist(xov_ref.dR.astype(float), bins=num_bins, density=False, facecolor='red',
-                                    alpha=0.3, range=[-1.*xlim, xlim])
-    plt.xlabel('dR (m)')
-    plt.ylabel('Probability')
+    # if isinstance(xov_ref, pd.DataFrame):
+    if True:
+        # xov_ref = xov_ref.loc[xov_ref.dR.abs() < 200]
+        n, bins, patches = plt.hist(xov_ref, bins=num_bins, density=False, facecolor='blue',
+                                    alpha=0.5, range=[-1.*xlim, xlim],label='post-fit')
+    plt.xlabel(r'$\nu$ (meters)')
+    plt.ylabel('Number of crossovers')
+    plt.legend(loc=1)
 
     mean_dR = xov_df.dR.mean()
     std_dR = xov_df.dR.std()
-    plt.title(r'Histogram of dR: $\mu=' + str(mean_dR) + ', \sigma=' + str(std_dR) + '$')
+    plt.title(r'Histogram of $\nu: \mu=%.2f' % mean_dR + ', \sigma=%.2f' % std_dR + '$')
     # Tweak spacing to prevent clipping of ylabel
     plt.subplots_adjust(left=0.15)
-    plt.savefig(XovOpt.get("tmpdir") + '/histo_dR_' + str(idx) + '.png')
+    plt.subplots_adjust(bottom=0.15)
+    plt.savefig('histo_dR_' + str(idx) + '.png')
+    plt.savefig('histo_dR_' + str(idx) + '.pdf',format='pdf')
+    # plt.savefig(XovOpt.get("tmpdir") + 'histo_dR_' + str(idx) + '.png')
     plt.clf()
     print("### plt_histo_dR: Plot saved to ", XovOpt.get("tmpdir") + '/histo_dR_' + str(idx) + '.png')
 
 
-def plt_geo_dR(sol, xov, truncation=None):
+def plt_geo_dR(sol, xov_df, truncation=None):
     # select only obs with dR<200 meters
-    xov_df = xov.xovers.copy()
+    # xov_df = xov.xovers.copy()
     if truncation:
         xov_df = xov_df.loc[xov_df.dR.abs()<truncation]
     # dR absolute value taken
@@ -65,11 +69,12 @@ def plt_geo_dR(sol, xov, truncation=None):
     # print(piv)
     # exit()
     sns.heatmap(piv, xticklabels=10, yticklabels=10)
-    plt.ylim(90,-5)
+    # plt.ylim(-90,90)
     plt.tight_layout()
     ax1.invert_yaxis()
     #         ylabel='Topog ampl rms (1st octave, m)')
-    fig.savefig(XovOpt.get("tmpdir") + '/mla_dR_' + sol + '.png')
+    # fig.savefig(XovOpt.get("tmpdir") + '/mla_dR_' + sol + '.png')
+    fig.savefig('mla_dR_' + sol + '.png')
     plt.clf()
     plt.close()
-    print("### plt_geo_dR: Plot saved to ", XovOpt.get("tmpdir") + '/mla_dR_' + sol + '.png')
+    print("### plt_geo_dR: Plot saved to ", XovOpt.get("tmpdir") + '/mla_dR_' + sol + '.png', format='png')
